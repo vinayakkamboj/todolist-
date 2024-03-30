be69ba8e4e84ebe59a47314e8e06e476ed6ae39b
Vinayak <94122024+vinayakkamboj@users.noreply.github.com>
2024-03-30 09:58:00 +0530

    Adding favicon and logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import AddTodo from './AddTodo';
 import TodoList from './TodoList';
+import Image from './images/logo192.png';
 
 function App() {
   return (
-    <div className="container mt-5 background-color: #f4f4f4;" >
-      <h1 className='text-center mb-5 text-xl font-weight-bold'>To Do List</h1>
+    <div className="container mt-5 background-color: #f4f4f4; " >
+      <img src={Image} alt="Description of the image" className="img-fluid justify-content-center align-items-center p-4" style={{ maxWidth: '200px' }}/>
+      {/* <h1 className='mb-5 text-xl font-weight-bold'>To Do List</h1> */}
       <AddTodo />
       <TodoList />
     </div>
